fe95e010cc9071aacc10a3b5a4b7f7616eb485b2
chris martinez and sheena nickerson <sheena@>
2016-08-18 10:45:27 -0700

    will return one M for every thousand for input

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,45 @@
-
 var romanNumerals = function(input){
+  if(isValid(input)){
+    ems = Math.floor(input/1000);
+    output = ems;
+    console.log("yo?");
+    for(var i =0; i<ems ; i++){
+      result += "M";
+    }
+    results1.push(result);
+  }//end if valid
+}//end romanNumerals
+
+
+var isValid = function(input){
   if((isNaN(input)) || (input > 3999 ) || (input < 0)){
-    $("#results").text("bad input");
+  return false;
   }
   else {
-    $("#results").text("good input");
+    return true;
   }
 }
+
+var result= "";
 var output;
 var input;
+var ems;
+var dees;
+var cees;
+var ells;
+var exes;
+var vees;
+var eyes;
+var results1 = []; //array with mulitiples of four
+var results2 = [] //finalized array without mulitples of four
 
 
 $(function(){
   $("form").submit(function(event){
     event.preventDefault();
     input = parseInt($("#input").val());
-    var results1 = []; //array with mulitiples of four
-    var results2 = [] //finalized array without mulitples of four
+
     output = romanNumerals(input);
-    $("#results").text(output);
+    $("#results").text(results1);
   });//end
 });
